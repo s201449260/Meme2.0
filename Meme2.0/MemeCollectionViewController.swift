c94a7d6fea44a10730196460883412966f6e679d
abdullah alammar <s201449260@kfupm.edu.sa>
2019-05-03 22:49:17 +0300

    fix2

--- Meme2.0/MemeCollectionViewController.swift
+++ Meme2.0/MemeCollectionViewController.swift
@@ -14,9 +14,6 @@ class MemeCollectionViewController: UICollectionViewController {
     // MARK: Properties
     
     
-    
-    private let reuseIdentifier = "MemeCollectionViewCell"
-    
     private let sectionInsets = UIEdgeInsets(top: 50.0,
                                              left: 20.0,
                                              bottom: 50.0,
